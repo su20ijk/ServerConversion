435ca12eefba65747a002e94e41245bc46fa822c
Unknown <vanessa.osterberg@outlook.com>
2018-06-22 11:26:57 -0500

    Everything Works

---
+++ ServerConversion/ServerConversion/Controllers/FeetController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServerConversion.Controllers
+{
+    public class FeetController : Controller
+    {
+        // GET: Feet
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FeeetToMiles()
+        {
+            return View(viewName: "FeeetToMiles", model: 0);
+        }
+
+        [HttpPost]
+        public ActionResult Converter(int Feet)
+        {
+            int Milesconvert = (Feet) / 5280;
+            return View(viewName: "FeeetToMiles", model: (Milesconvert));
+        }
+    }
+}
