32ec886f6d55c5dd22d6c5f5fdcf2920c329705f
Unknown <vanessa.osterberg@outlook.com>
2018-06-22 11:09:39 -0500

    Start

---
+++ ServerConversion/ServerConversion/Controllers/FeetController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServerUC.Controllers
+{
+    public class FeetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FeetToMiles()
+        {
+            return View(viewName: "FeetToMiles", model: " ");
+        }
+
+
+        [HttpPost]
+        public ActionResult Converter(int Feet, int Miles)
+        {
+            int Milesconvert;
+            int Feetconvert;
+            if (Feet != null)
+            {
+                Milesconvert = (Feet) / 5280;
+                return View(viewName: "FeetToMiles", model: (Milesconvert));
+            }
+            if (Miles != null)
+            {
+                Feetconvert = (Miles) * 5280;
+                return View(viewName: "FeetToMiles", model: (Feetconvert));
+            }
+            return View(viewName: "FeetToMiles", model: "Out of bounds Error");
+        }
+    }
+}
