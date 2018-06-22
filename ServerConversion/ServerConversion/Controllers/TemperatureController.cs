4dbdc29c0a45292bdf9005754a62ff5c0b4c05c4
Gene Lee <leexgenex@gmail.com>
2018-06-22 11:26:12 -0500

    cel to fah conversion

---
+++ ServerConversion/ServerConversion/Controllers/TemperatureController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServerConversion.Controllers
+{
+    public class TemperatureController : Controller
+    {
+        public ActionResult CtoF()
+        {
+            return View(viewName: "CtoF", model: 0.0);
+        }
+
+        [HttpPost]
+        public ActionResult CtoF(double inputC)
+        {
+            return View(viewName: "CtoF", model: inputC * (9 / 5) + 32);
+        }
+    }
+}
