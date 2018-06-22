7ffd934341f5bad3eac75a122a819b49a263035a
Andrew Kim <19kima@elmbrookstudents.org>
2018-06-22 11:17:02 -0500

    setup

---
+++ ServerConversion/ServerConversion/Controllers/KimController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServerUC.Controllers
+{
+    public class KimController : Controller
+    {
+        public ActionResult CmToM()
+        {
+            return View(viewName: "CmtoM", model: 0.0);
+        }
+
+        [HttpPost]
+        public ActionResult convert(double centimeter)
+        {
+            return View(viewName: "CmtoM", model: centimeter / 100);
+        }
+    }
+}
